3d3d855eb2a8759d814d6a3993d5e9733b2eaf60
topwebdev0612 <81346046+topwebdev0612@users.noreply.github.com>
2021-06-23 09:05:14 -0700

    last

--- pages/product-page/index.vue
+++ pages/product-page/index.vue
@@ -2,10 +2,10 @@
   <v-row align="center" class="mt-15">
     <v-col v-for="(item, i) in tbl_data" :key="i" class="d-flex child-flex" cols="12" sm="6" md="4" lg="3">
       <v-hover v-slot="{ hover }">
-        <v-card class="mx-auto" color="grey lighten-4" max-width="600">
+        <v-card class="mx-auto" color="grey lighten-4" max-width="600" height="100%">
           <v-img
             :aspect-ratio="16 / 9"
-            :src="'https://providr.uk' + item.image"
+            :src="item.image"
           >
             <template v-slot:placeholder>
               <v-row class="fill-height ma-0" align="center" justify="center">
@@ -15,37 +15,45 @@
                 ></v-progress-circular>
               </v-row>
             </template>
-            <v-expand-transition>
-              <div v-if="hover" class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal text-h2 white--text" style="height: 100%;">
-                £{{ item.price }}
-              </div>
-            </v-expand-transition>
           </v-img>
           <v-card-text class="pt-6" style="position: relative;">
-            <v-btn absolute color="orange" class="white--text" fab large right top @click="snackbar = true">
-              <v-icon>mdi-cart</v-icon>
-            </v-btn>
+            <v-checkbox :label="item.price+'£'" color="orange" hide-details @click="item_click(i)" v-model="item.selected"></v-checkbox>
             <div class="font-weight-light grey--text text-h6 mb-2">
-              For the perfect meal
             </div>
-            <h3 class="text-h4 font-weight-light orange--text mb-2">
+            <h3 class="text-h6 font-weight-light orange--text mb-2">
               {{ item.name }}
             </h3>
+            <div v-if='item.selected' class="d-flex justify-center align-center">
+              <v-btn class="mx-2" fab dark small color="primary" @click="calc(i, item.order==1?item.order:item.order--)">
+                <v-icon dark>
+                  mdi-minus
+                </v-icon>
+              </v-btn>
+              <input v-model="item.order" onKeyDown="return false" style="width:30px" type="number" min="1" max="10" />
+              <v-btn class="mx-2" fab dark small color="primary" @click="calc(i, item.order++)">
+                <v-icon dark>
+                  mdi-plus
+                </v-icon>
+              </v-btn>
+            </div>
           </v-card-text>
         </v-card>
       </v-hover>
     </v-col>
-    <v-snackbar v-model="snackbar" timeout= "-1" bottom right>
-    {{ text }}
-    <template v-slot:action="{ attrs }">
-      <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
-        Close
-      </v-btn>
-    </template>
-  </v-snackbar>
+    <v-snackbar v-model="snackbar" timeout="-1" bottom right multi-line shaped>
+      This is total cost:
+      £{{ total_cost.toFixed(2) }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="orange" text v-bind="attrs" @click="snackbar = false">
+          Order Now
+        </v-btn>
+        <!-- <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn> -->
+      </template>
+    </v-snackbar>
   </v-row>
 </template>
-
 
 <script>
 import Logo from "~/components/Logo.vue";
@@ -63,14 +71,58 @@ export default {
     return {
       tbl_data: [],
       snackbar: false,
-      text: `Hello, I'm a snackbar`,
+      text: "This is total cost",
+      check_model: [],
+      order_number: [],
+      total_cost:0,
+      own_cost: []
     };
   },
-  methods: {},
+  methods: {
+    item_click(index) {   //click the checkbox button
+      let selected = this.tbl_data[index].selected
+      console.log(selected)
+      if (selected == true) {
+        this.snackbar = true
+      }
+
+      if(!selected){
+        this.tbl_data[index].order = 0
+      }else if (selected) {
+        this.tbl_data[index].order = 1
+      }
+      const cost = this.tbl_data[index].price * this.tbl_data[index].order
+      this.own_cost[index] = selected ? cost : 0;
+      this.own_cost[index] = cost
+      const res = this.totalCost(this.own_cost)
+      this.total_cost = res
+      console.log("this is total cost:", res)
+    },
+    calc(index, order_count) {    //click the plus or minus button
+      const cost = this.tbl_data[index].price * this.tbl_data[index].order
+      this.own_cost[index] = cost
+      
+      const res = this.totalCost(this.own_cost)
+      this.total_cost = res
+      console.log("this is total cost:", res)
+    },
+    totalCost(prices){
+      let res = 0
+      for(let cost of prices) res += cost
+      return res;
+    }
+  },
   async mounted() {
     console.log("this is before all");
-    this.tbl_data = await getProductList(null);
-  }
+    var temp = await getProductList(null);
+    temp.map((t, index)=>{
+      t['order'] = 1;
+      t['selected'] = false;
+    })
+    this.tbl_data = temp;
+    for(let i =  0; i < this.tbl_data.length; i++) this.own_cost[i] = 0
+
+  },
 };
 </script>
 <style scoped>
@@ -81,5 +133,16 @@ export default {
   opacity: 0.9;
   position: absolute;
   width: 100%;
+}
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
 }
 </style>
